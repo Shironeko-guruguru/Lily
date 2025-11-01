29d946b59a878e91b6749d2592ae5ca4600b3587
Shironeko-guruguru <emavue953@gmail.com>
2025-11-01 12:41:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+window.onload = function() {
+  const canvas = document.getElementById("gameCanvas");
+  const ctx = canvas.getContext("2d");
+  if (!ctx) {
+        console.error("Canvasの2Dコンテキストが取得できませんでした。");
+        return;
+    }
+  
