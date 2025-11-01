a054b19b7464ff61cfa360aa198e26faf38d6758
Shironeko-guruguru <emavue953@gmail.com>
2025-11-01 15:15:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ window.onload = function() {
     }
   
   const playerImage = new Image();
+
+  const assets = {
+    images: {},
+    imageSources: {
+      c1:"UmidukaAoi.png"
+    },
+    load: function(onComplete) {
+      const sources = this.imageSources;
+      const images = this.images;
+      const numImages = Object.keys(sources).length;
+      let numLoaded = 0;
+      if (numImages === 0) {
+                onComplete();
+                return;
+      }
+      console.log("ローディング開始...")
+
+      for (const key in sources) {
+                const img = new Image();
+                img.src = sources[key];
+                
+                img.onload = () => {
+                    numLoaded++;
+                    images[key] = img;
+                    console.log(`  ${key} (${sources[key]}) ロード完了`);
+                    
+                    if (numLoaded === numImages) {
+                        console.log("ローディング完了");
+                        onComplete();
+                    }
+                };
+                img.onerror = () => {
+                    console.error(`${key} (${sources[key]}) のロードに失敗`);
+                    numLoaded++;
+                    if (numLoaded === numImages) {
+                        console.log("ローディング完了 (一部失敗あり)。");
+                        onComplete();
+                    }
+                };
+            }
+        }
+    };
